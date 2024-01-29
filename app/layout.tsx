45045be55a762ddd5e4cec0bb072d1a6dc308431
ronkips <kipshillash@gmail.com>
2024-01-29 12:15:57 +0300

    layout update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,52 @@
+"use client";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import TopNav from "@/components/Navbar/TopNav";
+import MobileNavigation from "@/components/Navbar/Mobile";
+import DesktopNavigation from "@/components/Navbar/Desktop";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+ const metadata: Metadata = {
   title: "MjengoSmart",
-  description: "Your reliable construction partner",
+  description: "Your reliable construction partner"
 };
 
 export default function RootLayout({
   children,
+  auth
 }: Readonly<{
   children: React.ReactNode;
+  auth: any;
 }>) {
+  type Props = {
+    auth: any;
+    header: any;
+    children: React.ReactNode;
+  };
+
+  // const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <html lang="en">
       <body className={inter.className}>
+        {/* below is the part taht be a shared layout, uncoment it and play with it  */}
+        {/* <nav>
+          <TopNav auth={auth} user={auth?.user} />
+          <div className="flex">
+            <MobileNavigation
+              user={auth?.user}
+              sidebarOpen={false}
+              setSidebarOpen={() => {}}
+            />
+
+            <DesktopNavigation user={auth?.user} />
+          </div>
+        </nav> */}
+
         {children}
-        </body>
+      </body>
     </html>
   );
 }
